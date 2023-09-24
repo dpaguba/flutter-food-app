5a0fe36112f1ab1ce5b0bc9d1f231bc9b5bb9192
Dmytro Pahuba <dpaguba@gmail.com>
2023-09-24 21:48:28 +0200

    appbar done so far

--- fitness/lib/pages/home.dart
+++ fitness/lib/pages/home.dart
@@ -7,58 +7,62 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Breakfest",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+      appBar: _callAppBar(),
+    );
+  }
+
+  AppBar _callAppBar() {
+    return AppBar(
+      title: const Text(
+        "Breakfest",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: .0,
+      leading: GestureDetector(
+        onTap: () {
+          // call function for arrow left button
+        },
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(1, 247, 248, 248),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/arrowLeft.svg',
+            height: 20,
+            width: 20,
           ),
         ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        elevation: .0,
-        leading: GestureDetector(
+      ),
+      actions: [
+        GestureDetector(
           onTap: () {
-            // call function for arrow left button
+            // call functions for 2 dots button
           },
           child: Container(
             margin: const EdgeInsets.all(10),
             alignment: Alignment.center,
+            width: 40,
             decoration: BoxDecoration(
               color: const Color.fromARGB(1, 247, 248, 248),
               borderRadius: BorderRadius.circular(10),
             ),
             child: SvgPicture.asset(
-              'assets/icons/arrowLeft.svg',
-              height: 20,
-              width: 20,
+              'assets/icons/dots.svg',
+              height: 7,
+              width: 7,
             ),
           ),
         ),
-        actions: [
-          GestureDetector(
-            onTap: () {
-              // call functions for 2 dots button
-            },
-            child: Container(
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 40,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(1, 247, 248, 248),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: SvgPicture.asset(
-                'assets/icons/dots.svg',
-                height: 7,
-                width: 7,
-              ),
-            ),
-          )
-        ],
-      ),
+      ],
     );
   }
 }
